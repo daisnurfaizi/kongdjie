590660c58231f85f6cb95f9634a886ea52895d78
unknown <yutoumar43@gmail.com>
2021-04-03 19:08:16 +0700

    editing

--- app/views/home/index2.php
+++ app/views/home/index2.php
@@ -1,22 +1,140 @@
+
 <!doctype html>
 <html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-<head>
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+    <LInk rel="stylesheet" href="<?= BASEURL, PORT, LOCATION; ?>/css/style2.css">
+    <link rel="icon" type="image/png" href="<?= BASEURL, PORT, LOCATION; ?>/img/logo.png">
+
+    <title>Kong Djie Coffee</title>
+  </head>
+  <body>
+    <!-- navbar -->
+    <div class="logatas">
+      <img src="<?= BASEURL, PORT, LOCATION; ?>/img/logo.png">
+      <p>CV. KONDJIE HAMPARAN RASA</p>    
+    </div>
+    <nav class="navbar navbar-expand-lg navbar-light ">
+      <div class="container">
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup"  aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+          <div class="navbar-nav mx-auto">
+            <a class="nav-link" href="#home">home</a>
+            <a class="nav-link" href="#profile">profile</a>
+            <a class="nav-link" href="#product">product</a>
+            <a class="nav-link" href="#franchise">franchise</a>
+            <a class="nav-link" href="#about">about</a>
+            <a class="nav-link" href="#location">location</a>
+            <a class="nav-link" href="#content">Contact</a>
+          </div>
+        </div>
+      </div>
+    </nav>
+    <!-- akhir navbar -->
+
+    
+    <!-- Jumbotron Header  -->
+    <section class="header" id="home"> 
+      <div class="jumbotron jumbotron-fluid">
+        <div class="container">
+          <img src="<?= BASEURL, PORT, LOCATION; ?>/img/logorev.png">
+          <p> Ingin Bermitra <br> Klik : </p>
+          <a href="" class="btn joinus"> Join Us </a>
+          <a href="" class="btn tentang"> About Us </a>
+          <div class="medsos">
+            <p>kongdjieoffice</p> 
+            <img src="<?= BASEURL, PORT, LOCATION; ?>/img/0111-instagram.png" class="instagram">
+            <img src="<?= BASEURL, PORT, LOCATION; ?>/img/031-facebook.png" class="facebook">
+
+            <div class="ornamen">
+              <img src="<?= BASEURL, PORT, LOCATION; ?>/img/Bijay Coffe Back Atas.png">
+            </div>    
+            <div class="ornamen1">
+              <img src="<?= BASEURL, PORT, LOCATION; ?>/img/Bijay Coffe Back Bawah.png">
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <!-- Akhir Jumbotron Header-->
+
+    <!-- Jumbotron Profile -->
+    <section class="isi" id="profile">
+      <div class="container-satu">
+        <div class="abot">
+          <p> Kong Djie Coffee merupakan usaha kopi yang berdiri sejak 1943 yang berlokasi  di TanjungPandan , Belitung. Saat ini Kong Djie di dukung lebih 100 outlet cafe dan resto.  Kong Djie berkomitmen untukselalu memenuhi kebutuhan dan kepuasan konsumen,  tidak hanya dengan menyediakan produk dengan kualitasterbaik, namun juga memberikan solusi yang tepat bagi setiap pangsa pasar kami. </p>
+          
+          <a href="" class="btn view_detail"> View Details </a>
+        </div>
+        <div class="imageprof">
+          <img src="<?= BASEURL, PORT, LOCATION; ?>/img/gawean1.png">
+        </div> 
+      </div>
+      
+        <!-- <div class="teksvisi">
+          <h3> Visi & Misi </h3>
+        </div>
+        
+        <div class="contenvisimisi">
+          <div class="visi">
+            <p> menjadi perusahaan terbaik yang mampu memberikan rasa yang berkualitas </p>
+          </div>
+          <div class="misi">
+            <p><a href=""> manajemen dengan integritas tinggi </a></p>
+            <p><a href=""> mampu memperoleh laba yang baik </a></p>
+            <p><a href=""> pengabdian untuk relasi dan konsumen </a></p>
+            <p><a href=""> inovasi tanpa henti </a></p>
+            <p><a href=""> standar etika yang tinggi </a></p>
+          </div>
+        </div> -->
+  
+        <!-- <div class="ornamenabot">
+          <img src="img/bijay coffe menu prodile.png">
+        </div>  -->
+    <!-- </section> -->
+    <!-- Akhir Jumbotron Profile -->
+
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: jQuery and Bootstrap Bundle (includes Popper) -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+
+    <!-- Option 2: jQuery, Popper.js, and Bootstrap JS
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js" integrity="sha384-w1Q4orYjBQndcko6MimVbzY0tgp4pWB4lZ7lr30WKz0vr/aWKhXdBNmNb5D92v7s" crossorigin="anonymous"></script>
+    -->
+  </body>
+</html>
+
+
+<!-- <!doctype html> -->
+<!-- <html lang="en"> -->
+
+<!-- <head> -->
   <!-- Required meta tags -->
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <!-- <meta charset="utf-8"> -->
+  <!-- <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"> -->
 
   <!-- Bootstrap CSS -->
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
-  <link rel="stylesheet" href="<?= BASEURL, PORT, LOCATION; ?>/css/style.css">
-  <link rel="icon" type="<?= BASEURL, PORT, LOCATION; ?>/image/png" href="logo.png">
-  <title>KongDjie Coffee</title>
-</head>
+  <!-- <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous"> -->
+  <!-- <link rel="stylesheet" href="<?= BASEURL, PORT, LOCATION; ?>/css/style.css"> -->
+  <!-- <link rel="icon" type="<?= BASEURL, PORT, LOCATION; ?>/image/png" href="logo.png"> -->
+  <!-- <title>KongDjie Coffee</title> -->
+<!-- </head> -->
 
-<body>
+<!-- <body> -->
 
   <!-- Navbar  -->
-  <section class="home" id="home">
+  <!-- <section class="home" id="home">
     <div class="toggle" onclick="toggleMenu();"></div>
     <div class="navbar center">
       <ul>
@@ -41,11 +159,11 @@
         </div>
       </ul>
     </div>
-  </section>
+  </section> -->
 
   <!-- Akhir Navbar -->
   <!-- about -->
-  <section class="about" id="about">
+  <!-- <section class="about" id="about">
     <div class="container">
       <div class="ornamen">
         <img src="img/Bijay Coffee Mid.png">
@@ -65,11 +183,11 @@
         <p>Produk-produk Kong Djie telah tersebar di outlet-outlet seluruh kota di Indonesia. </br> Penyebaran tidak hanya ke kota-kota tetapi telah sampai ke pelosok desa, berkat kerjasama mitra kerja.</p>
       </div>
     </div>
-  </section>
+  </section> -->
   <!-- akhir about -->
 
   <!-- Location -->
-  <section class="location" id="location">
+  <!-- <section class="location" id="location">
     <div class="jumbotron">
       <div class="container">
         <h3 class="display-4">Info lokasi mitra kongdjie coffee</h3>
@@ -117,11 +235,11 @@
         </div>
       </div>
     </div>
-  </section>
+  </section> -->
   <!-- Akhit Location -->
 
   <!-- Contact  -->
-  <section class="contact" id="contact">
+  <!-- <section class="contact" id="contact">
     <div class="container">
       <div class="row">
         <div class="col-sm">
@@ -167,17 +285,17 @@
       </div>
     </div>
     </div>
-  </section>
+  </section> -->
   <!--Akhir Contact  -->
 
   <!-- Copyright -->
-  <footer>
+  <!-- <footer>
     <div class="footer">
       <p>KongDjieCoffee&copy;Copyright2021</p>
     </div>
-  </footer>
+  </footer> -->
   <!-- Akhir Copyright -->
-  <script type="text/javascript">
+  <!-- <script type="text/javascript">
     window.addEventListener('scroll', function() {
       var header = document.querySelector('header');
       header.classList.toggle('sticky', window.scrollY > 0);
@@ -189,19 +307,19 @@
       menuToggle.classList.toggle('active');
       menu.classList.toggle('active');
     }
-  </script>
+  </script> -->
 
   <!-- Optional JavaScript; choose one of the two! -->
 
   <!-- Option 1: jQuery and Bootstrap Bundle (includes Popper) -->
-  <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+  <!-- <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script> -->
 
   <!-- Option 2: jQuery, Popper.js, and Bootstrap JS
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js" integrity="sha384-w1Q4orYjBQndcko6MimVbzY0tgp4pWB4lZ7lr30WKz0vr/aWKhXdBNmNb5D92v7s" crossorigin="anonymous"></script>
     -->
-</body>
+<!-- </body>
 
-</html>
+</html> -->
