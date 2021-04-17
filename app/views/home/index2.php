c34e1959d96eceab2bc83a1c370c7da2447fd632
eliot <daisnurfaizi>
2021-04-17 15:07:49 +0700

    fix bug

--- app/views/home/index2.php
+++ app/views/home/index2.php
@@ -8,7 +8,7 @@
 
   <!-- Bootstrap CSS -->
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
-  <LInk rel="stylesheet" href="<?= BASEURL, PORT, LOCATION; ?>/css/style2.css">
+  <LInk rel="stylesheet" type="text/css" href="<?= BASEURL, PORT, LOCATION; ?>/css/style2.css">
   <link rel="icon" type="image/png" href="<?= BASEURL, PORT, LOCATION; ?>/img/logo.png">
   <style>
     #more {
