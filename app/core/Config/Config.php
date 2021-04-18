e635b1c5d04cc6ec4e26a33798489efcf50ccf62
eliot <daisnurfaizi>
2021-04-19 01:07:24 +0700

    heroku db posgre

--- app/core/Config/Config.php
+++ app/core/Config/Config.php
@@ -6,7 +6,8 @@ define('PORT', '');
 //folder web
 define('LOCATION', '/public');
 //database atur disini
-define('DB_HOST', 'localhost');
-define('DB_USER', 'root');
+define('DB_HOST', 'ec2-107-22-83-3.compute-1.amazonaws.com');
+define('DB_USER', 'xettshdiuagaqj');
 define('DB_PASS', '');
-define('DB_NAME', 'kongdji');
+define('DB_PORT', '5432');
+define('DB_NAME', 'd4mub9cu38n454');
