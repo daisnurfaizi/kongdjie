715e7b5a47b073b1c15df0cadece330e4d965b8d
eliot <daisnurfaizi>
2021-04-19 01:09:18 +0700

    heroku db posgre password

--- app/core/Config/Config.php
+++ app/core/Config/Config.php
@@ -8,6 +8,6 @@ define('LOCATION', '/public');
 //database atur disini
 define('DB_HOST', 'ec2-107-22-83-3.compute-1.amazonaws.com');
 define('DB_USER', 'xettshdiuagaqj');
-define('DB_PASS', '');
+define('DB_PASS', 'd2892b8927e6ae2fa8d4d39983af26a09532e48bda8e5a454eb02b97d1010cb2');
 define('DB_PORT', '5432');
 define('DB_NAME', 'd4mub9cu38n454');
