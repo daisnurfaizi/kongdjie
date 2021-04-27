54b6d62f8cdb3d9927407217eaaad9eba026ae8a
eliot <daisnurfaizi>
2021-04-28 01:45:21 +0700

    fix bug

--- app/views/Admin/login/index.php
+++ app/views/Admin/login/index.php
@@ -8,8 +8,9 @@
   <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
   <meta name="generator" content="Jekyll v4.0.1">
   <title><?= $data['judul']; ?></title>
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
 
-  <link rel="stylesheet" href="<?= BASEURL, PORT, LOCATION; ?>/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+  <!-- <link rel="stylesheet" href="<?= BASEURL, PORT, LOCATION; ?>/css/bootstrap.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous"> -->
   <script src="<?= BASEURL, PORT, LOCATION; ?>/js/jquery-3.5.1.min.js"></script>
 
   <!-- Bootstrap core CSS -->
--- app/views/home/index2.php
+++ app/views/home/index2.php
@@ -1,5 +1,4 @@
-<?= BASEURL, PORT, LOCATION; ?>
-<!doctype html>
+<!-- <!doctype html> -->
 <html lang="en">
 
 <head>
@@ -8,9 +7,10 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
 
   <!-- Bootstrap CSS -->
+  <!-- <LInk rel="stylesheet" href="<?= BASEURL, PORT, LOCATION ?>/css/bootstrap.css"> -->
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
-  <LInk rel="stylesheet" href="<?= BASEURL, PORT, LOCATION; ?>/style.css">
-  <link rel="icon" type="image/png" href="<?= BASEURL, PORT, LOCATION; ?>/img/logo.png">
+  <LInk rel="stylesheet" href="<?= BASEURL, PORT, LOCATION ?>/css/style.css">
+  <link rel="icon" type="image/png" href="<?= BASEURL, PORT, LOCATION ?>/img/logo.png">
 
   <title>Kong Djie Coffe!</title>
 </head>
@@ -18,7 +18,7 @@
 <body>
   <!-- navbar -->
   <div class="logatas">
-    <img src="<?= BASEURL, PORT, LOCATION; ?>/img/logo.png">
+    <img src="<?= BASEURL, PORT, LOCATION ?>/img/logo.png">
     <p>CV. KONDJIE HAMPARAN RASA</p>
   </div>
   <nav class="navbar navbar-expand-lg navbar-light sticky-top">
@@ -48,7 +48,7 @@
       <div class="container text-center">
         <div class="row">
           <div class="col-sm-12">
-            <img src="<?= BASEURL, PORT, LOCATION; ?>/img/LogoKongDjieHome.png" alt="Logo-KongDjie">
+            <img src="<?= BASEURL, PORT, LOCATION ?>/img/LogoKongDjieHome.png" alt="Logo-KongDjie">
           </div>
         </div>
 
@@ -64,13 +64,13 @@
 
         <div class="row">
           <div class="col-md-5">
-            <a href="profile"><img src="<?= BASEURL, PORT, LOCATION; ?>/img/0111-instagram.png" alt="instagram-Logo" class="instagram"></a>
+            <a href="profile"><img src="<?= BASEURL, PORT, LOCATION ?>/img/0111-instagram.png" alt="instagram-Logo" class="instagram"></a>
           </div>
           <div class="col-md-2">
             <h4> kongdjieoffice </h4>
           </div>
           <div class="col-md-5">
-            <a href=""><img src="<?= BASEURL, PORT, LOCATION; ?>/img/031-facebook.png" alt="Facebook-Logo" class="facebook"></a>
+            <a href=""><img src="<?= BASEURL, PORT, LOCATION ?>/img/031-facebook.png" alt="Facebook-Logo" class="facebook"></a>
           </div>
         </div>
 
@@ -90,38 +90,32 @@
     <div class="container text-center">
       <div class="row">
         <div class="col-sm-6 paragraf">
-          <p> Kong Djie Coffee merupakan usaha kopi yang berdiri sejak 1943 yang berlokasi di Tanjung Pandan , Belitung. Saat ini Kong Djie di dukung lebih 100 outlet cafe dan resto. Kong Djie berkomitmen untuk selalu memenuhi kebutuhan dan kepuasan konsumen, tidak hanya dengan menyediakan produk dengan kualitas terbaik, namun juga memberikan solusi yang tepat bagi setiap pangsa pasar kami. </p>
-          <a href="" class="btn viewdetail" data-bs-toggle="modal" data-bs-target="#exampleModal"> View Detail </a>
-          <div class="modal" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-            <div class="modal-dialog modal-dialog-scrollable">
-              <div class="modal-content">
-                <div class="modal-header">
-                  <h3>Sejarah KongDjie Coffee</h3>
-                  <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                </div>
-                <div class="modal-body">Kong Djie Coffee merupakan usaha kopi yang berdiri sejak 1943 yang
-                  berlokasi di Tanjung Pandan , Belitung. Kong djie coffee yang di awali berbentuk kedai kopi
-                  yang hingga saat ini berubah menjadi cafe dan resto di setiap Kota Besar di Indonesia. Saat
-                  ini Kong Djie di dukung lebih 100 outlet cafe dan resto. <br><br>
-                  Dan pada Tahun 2021 dengan memperluas usaha nya, Kong Djie meramba usahanya di sektor Retail
-                  dan Distribusi dengan mendirikan CV.Kong Djie Hamparan Rasa. Kong Djie berkomitmen untuk
-                  selalu memenuhi kebutuhan dan kepuasan konsumen, tidak hanya dengan menyediakan produk dengan
-                  kualitas terbaik, namun juga memberikan solusi yang tepat bagi setiap pangsa pasar kami.
-                  <br><br>
-                  Dengan pengalaman yang cukup mumpuni Kong Djie telah memiliki kepekaan untuk memberikan
-                  Kualitas dan rasa atas setiap kebutuhan konsumen yang terus berkembang. Pengetahuan ini
-                  membuat kami mampu untuk memberikan solusi yang tepat bagi mitra kami dalam memenuhi
-                  keinginan konsumen yang semakin kompleks. <br><br>
-                  Seluruh Produk kami terjamin keamanan dan mutunya karena sebelum beredar telah diperiksa dan
-                  didaftarkan ke Balai Pengawasan Obat dan Makanan (BPOM).
-                </div>
-              </div>
-            </div>
-          </div>
+          <p>Kong Djie Coffee merupakan usaha kopi yang berdiri sejak 1943 yang berlokasi di Tanjung Pandan , Belitung. Saat ini Kong Djie di dukung lebih 100 outlet cafe dan resto. Kong Djie berkomitmen untuk selalu memenuhi kebutuhan dan kepuasan konsumen, tidak hanya dengan menyediakan produk dengan kualitas terbaik, namun juga memberikan solusi yang tepat bagi setiap pangsa pasar kami.<span id="dots">...</span><span id="more"> Dengan pengalaman yang cukup mumpuni Kong Djie telah memiliki kepekaan untuk memberikan Kualitas dan rasa atas setiap kebutuhan konsumen yang terus berkembang. Pengetahuan ini membuat kami mampu untuk memberikan solusi yang tepat bagi mitra kami dalam memenuhi keinginan konsumen yang semakin kompleks. Seluruh Produk kami terjamin keamanan dan mutunya karena sebelum beredar telah diperiksa dan didaftarkan ke Balai Pengawasan Obat dan Makanan (BPOM).</span></p>
+          <button class="btn viewdetail" onclick="myFunction()" id="myBtn">Read more</button>
+          <!-- readmore -->
+          <script>
+            function myFunction() {
+              var dots = document.getElementById("dots");
+              var moreText = document.getElementById("more");
+              var btnText = document.getElementById("myBtn");
+
+              if (dots.style.display === "none") {
+                dots.style.display = "inline";
+                btnText.innerHTML = "Read more";
+                moreText.style.display = "none";
+              } else {
+                dots.style.display = "none";
+                btnText.innerHTML = "Read less";
+                moreText.style.display = "inline";
+              }
+            }
+          </script>
+          <!-- readmore -->
+          <!-- <button class="btn viewdetail" onclick="myFunction()" id="myBtn"> Read More </button> -->
         </div>
 
         <div class="col-sm-6">
-          <img src="<?= BASEURL, PORT, LOCATION; ?>/img/gawean1.png" alt="viewdetail" class="gambarprofile">
+          <img src="<?= BASEURL, PORT, LOCATION ?>/img/gawean1.png" alt="viewdetail" class="gambarprofile">
         </div>
       </div>
 
@@ -157,7 +151,7 @@
 
         <div class="col produk">
           <div class="card">
-            <img src="<?= BASEURL, PORT, LOCATION; ?>/img/KongDjie_Gayo.png" class="card-img-top" alt="Gayo-Coffee">
+            <img src="<?= BASEURL, PORT, LOCATION ?>/img/KongDjie_Gayo.png" class="card-img-top" alt="Gayo-Coffee">
             <div class="card-body">
               <h5 class="card-title">Gayo Coffee</h5>
               <p class="card-text"> Rp. 100.000 ,- </p>
@@ -170,7 +164,7 @@
 
         <div class="col produk">
           <div class="card ">
-            <img src="<?= BASEURL, PORT, LOCATION; ?>/img/KongDjie_Mandheling.png" class="card-img-top" alt="Mandheling-Coffee">
+            <img src="<?= BASEURL, PORT, LOCATION ?>/img/KongDjie_Mandheling.png" class="card-img-top" alt="Mandheling-Coffee">
             <div class="card-body">
               <h5 class="card-title"> Mandheling Coffee </h5>
               <p class="card-text"> Rp. 100.000 ,- </p>
@@ -182,7 +176,7 @@
         </div>
         <div class="col produk">
           <div class="card ">
-            <img src="<?= BASEURL, PORT, LOCATION; ?>/img/KongDjie_Toraja.png" class="card-img-top" alt="Toraja-Coffee">
+            <img src="<?= BASEURL, PORT, LOCATION ?>/img/KongDjie_Toraja.png" class="card-img-top" alt="Toraja-Coffee">
             <div class="card-body">
               <h5 class="card-title"> Toraja Coffee </h5>
               <p class="card-text"> Rp. 100.000 ,- </p>
@@ -194,7 +188,7 @@
         </div>
         <div class="col produk">
           <div class="card ">
-            <img src="<?= BASEURL, PORT, LOCATION; ?>/img/KongDjie_Kopi.png" class="card-img-top" alt="Original-Coffee">
+            <img src="<?= BASEURL, PORT, LOCATION ?>/img/KongDjie_Kopi.png" class="card-img-top" alt="Original-Coffee">
             <div class="card-body">
               <h5 class="card-title"> Original Coffee </h5>
               <p class="card-text"> Rp. 100.000 ,- </p>
@@ -207,7 +201,7 @@
 
         <div class="col produk">
           <div class="card ">
-            <img src="<?= BASEURL, PORT, LOCATION; ?>/img/KongDjie_Coklat.png" class="card-img-top" alt="Cokelat-Coffee">
+            <img src="<?= BASEURL, PORT, LOCATION ?>/img/KongDjie_Coklat.png" class="card-img-top" alt="Cokelat-Coffee">
             <div class="card-body">
               <h5 class="card-title"> Cokelat Coffee </h5>
               <p class="card-text"> Rp. 100.000 ,- </p>
@@ -224,7 +218,7 @@
         </div>
       </div>
     </div>
-    <img src="img/Bijay Coffe Back Bawah.png" class="ornamen3" alt="Biji-Coffee">
+    <img src="<?= BASEURL, PORT, LOCATION ?>/img/Bijay Coffe Back Bawah.png" class="ornamen3" alt="Biji-Coffee">
   </section>
 
 
@@ -242,7 +236,7 @@
             <div class="col-lg info2 text-center">
               <p> Hubungi Kami : </p>
               <a href="">
-                <img src="<?= BASEURL, PORT, LOCATION; ?>/img/whatsapp.png" alt="WhatsApp">
+                <img src="<?= BASEURL, PORT, LOCATION ?>/img/whatsapp.png" alt="WhatsApp">
               </a>
             </div>
             <div class="col-lg">
@@ -290,10 +284,10 @@
       </div>
     </div>
     <div class="ornamen-mid">
-      <img src="<?= BASEURL, PORT, LOCATION; ?>/img/Bijay Coffee Mid.png" alt="Biji-Coffew">
+      <img src="<?= BASEURL, PORT, LOCATION ?>/img/Bijay Coffee Mid.png" alt="Biji-Coffew">
     </div>
     <div class="ornamen-mid1">
-      <img src="<?= BASEURL, PORT, LOCATION; ?>img/Bijay Coffe Back Bawah.png" alt="Biji-Coffee">
+      <img src="<?= BASEURL, PORT, LOCATION ?>/img/Bijay Coffe Back Bawah.png" alt="Biji-Coffee">
     </div>
     </div>
   </section>
@@ -305,7 +299,7 @@
       <h3 class="display-4">Info lokasi mitra kongdjie coffee</h3>
       <div class="row backlok justify-content-center">
         <div class="col">
-          <img src="img/gawean1.png">
+          <img src="<?= BASEURL, PORT, LOCATION ?>/img/gawean1.png">
         </div>
       </div>
       <div class="row tekswilayah justify-content-center">
@@ -355,19 +349,19 @@
       <div class="row">
         <div class="col-md-5">
           <div class="contact1">
-            <img src="img/pin-512.png" alt="Maps-Logo" class="float-left">
+            <img src="<?= BASEURL, PORT, LOCATION ?>/img/pin-512.png" alt="Maps-Logo" class="float-left">
             <p>Taman Palem Mutiara Blok B7 no.30 Cengkareng Timur, Jakarta Barat</p>
           </div>
           <div class="contact1">
-            <img src="img/email.png" alt="Email-Logo" class="float-left" id="email">
+            <img src="<?= BASEURL, PORT, LOCATION ?>/img/email.png" alt="Email-Logo" class="float-left" id="email">
             <p>kngdjcr43@gmail.com</p>
           </div>
           <div class="contact1">
-            <img src="img/PHONE.png" alt="Phone-Logo" class="float-left">
+            <img src="<?= BASEURL, PORT, LOCATION ?>/img/PHONE.png" alt="Phone-Logo" class="float-left">
             <p>(021-22654327)</p>
           </div>
           <div class="contact1">
-            <img src="img/003-whatsapp.png" alt="WhatsApp" class="float-left">
+            <img src="<?= BASEURL, PORT, LOCATION ?>/img/003-whatsapp.png" alt="WhatsApp" class="float-left">
             <p>(+62-12334567891011)</p>
           </div>
         </div>
@@ -388,9 +382,9 @@
           <div class="sertifikat">
             <p>SERTIFIKAT</p>
             <div class="box">
-              <img src="img/100PERSERN.png" alt="100%-Indonesia" id="100indo">
-              <img src="img/mui.png" alt="MUI" id="mui">
-              <img src="img/VPOM.png" alt="BPOM" id="bpom">
+              <img src="<?= BASEURL, PORT, LOCATION ?>/img/100PERSERN.png" alt="100%-Indonesia" id="100indo">
+              <img src="<?= BASEURL, PORT, LOCATION ?>/img/mui.png" alt="MUI" id="mui">
+              <img src="<?= BASEURL, PORT, LOCATION ?>/img/VPOM.png" alt="BPOM" id="bpom">
             </div>
           </div>
         </div>
@@ -408,13 +402,14 @@
   <!-- Optional JavaScript; choose one of the two! -->
 
   <!-- Option 1: Bootstrap Bundle with Popper -->
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>
+  <script src="<?= BASEURL, PORT, LOCATION ?>/https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>
   <script>
-    scr = "js/jquery-3.6.0.min.js"
+    scr = "<?= BASEURL, PORT, LOCATION ?>/js/jquery-3.6.0.min.js"
   </script>
   <script>
-    scr = "js/KongDjie.js"
+    scr = "<?= BASEURL, PORT, LOCATION ?>/js/KongDjie1.js"
   </script>
+
 
   <!-- Option 2: Separate Popper and Bootstrap JS -->
   <!--
